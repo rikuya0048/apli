52f9aa29f807b2df003a965a83b4ab34f3e524b7
rikuya0048 <rikuya0048@gmail.com>
2020-02-23 20:35:36 +0900

    コミット１

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -6,3 +6,34 @@ $(function () {
     $('.icon_link_tab').removeClass('open');
   });
 });
+
+$(function () {
+  $('i#user').on('click', () => {
+    $('.head-user-hidden').show('');
+  });
+});
+
+$(function () {
+  $('i#search').on('click', () => {
+    $('.header-search-form').show('');
+    $('i#search').remove('');
+  });
+});
+
+
+
+
+
+
+
+
+// $(function () {
+//   $('button').on('click', () => {
+//     $('.menu-bar').show('');
+//   });
+
+//   $('.menu-btn').on('click', () => {
+//     $('.menu-btn').hide('');
+//     $('.menu-bar').hide('')
+//   });
+// });
