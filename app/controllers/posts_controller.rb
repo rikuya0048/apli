0dcff77c10a65810f6e1a0a0aabd9fd20433d294
rikuya0048 <rikuya0048@gmail.com>
2020-03-03 14:04:39 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 before_action :move_to_index, except: [:index, :show, :search]
 
 def index
-  @posts = Post.includes(:user).order("created_at DESC")
+  @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
 end
 
 def create
