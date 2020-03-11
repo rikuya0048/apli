0d251bd776986b7c02f66de8d7bb33228f81e45c
rikuya0048 <rikuya0048@gmail.com>
2020-03-11 12:28:20 +0900

    enviromentsスペース
    スペース修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,6 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-　
   server '3.114.144.37', user: 'ec2-user', roles: %w{app db web}
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
