5de15f465f553be7c196d4f048709239ca5f4a2d
rikuya0048 <rikuya0048@gmail.com>
2020-05-07 15:16:28 +0900

    usercontroller修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,9 +38,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end
 
   def user_params
